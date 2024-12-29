88f6ec26f1b6da871f57e1587c0d84aff36fb292
Dan Sylvain <dansylvain84@gmail.com>
2024-12-29 13:28:21 +0100

    success

--- srcs/requirements/mariadb/conf/init.sql
+++ srcs/requirements/mariadb/conf/init.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE IF NOT EXISTS wp_database;
+USE wp_database;
 CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'wp_password';
 GRANT ALL PRIVILEGES ON wp_database.* TO 'wp_user'@'%';
+SOURCE /docker-entrypoint-initdb.d/dump.sql
 FLUSH PRIVILEGES;
